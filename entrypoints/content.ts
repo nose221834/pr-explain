0f34ff231b3a3569e4c932e5fbcb03162b30cc1c
nose221834 <iatyy01151821@gmail.com>
2025-12-15 02:26:45 +0900

    イメージを記述

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -8,3 +8,37 @@ export default defineContentScript({
     console.log("title:", title);
   },
 });
+
+// PRの差分をこんな感じで持てたら後から使いやすようと妄想中
+// {
+//   "src/foo.js": {
+//     "old": [
+//       { "line": 10, "text": "function foo() {", "type": "context" },
+//       { "line": 11, "text": "  return 1;", "type": "delete" },
+//       { "line": 12, "text": "}", "type": "context" }
+//     ],
+//     "new": [
+//       { "line": 10, "text": "function foo() {", "type": "context" },
+//       { "line": 11, "text": "  return 2;", "type": "change" },
+//       { "line": 12, "text": "}", "type": "context" }
+//     ]
+//   }
+// }
+//
+// もっと発展させて、hunksを持ってもいいね（連続した行の変更を一つのhunkとして扱う）
+// {
+//   "file.js": {
+//     "hunks": [
+//       {
+//         "oldStart": 12,
+//         "oldEnd": 14,
+//         "newStart": 12,
+//         "newEnd": 15,
+//         "lines": [
+//           { "type": "context", "oldLine": 12, "newLine": 12, "text": "..." },
+//           ...
+//         ]
+//       }
+//     ]
+//   }
+// }
