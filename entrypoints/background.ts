e8ecbc4df87fb227945bc54ff9a47c25993a89d9
nose221834 <iatyy01151821@gmail.com>
2025-12-15 01:58:50 +0900

    PRのページを開くように指定

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,8 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+  if (import.meta.env.DEV) {
+    browser.tabs.create({
+      url: "https://github.com/nose221834/pr-explain/pull/1/changes",
+    });
+  }
 });
